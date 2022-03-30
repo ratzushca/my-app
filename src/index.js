eda7cc3d420da633264e8f2092bc4dcbbc768235
ratzushca <93014061+ratzushca@users.noreply.github.com>
2022-03-30 11:58:23 -0700

    ui work

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import Nav from './Nav';
+import SearchMovie from './SearchMovies';
+import '../src/style.css';
 
+class Main extends React.Component {
+  render() {
+    return (
+      <div className="container">
+      <h1 className="title">React Movie Search!</h1>
+      <SearchMovie />
+      </div>
+    );
+  }
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Nav />
-    <App />
-    
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<Main />, document.getElementById('root'));
 
